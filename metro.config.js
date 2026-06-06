2e1913830e1a3a521cd66279d62abd1309f39b27
E-R-Briggs <ellisrb1272@gmail.com>
2026-06-06 20:00:44 +0100

    Add web source extension and wasm support for SQL.js on mobile web

--- metro.config.js
+++ metro.config.js
@@ -3,5 +3,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 config.resolver.assetExts.push('wasm');
+config.resolver.sourceExts.push('web');
 
 module.exports = config;
